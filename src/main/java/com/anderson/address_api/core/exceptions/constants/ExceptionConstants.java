d300dc185db94a99dd07e7597fbaeaff18edb3ba
Anderson Palmerim <andersonp.viana.contato@gmail.com>
2024-08-30 14:21:26 -0300

    feat: creating other constants

--- src/main/java/com/anderson/address_api/core/exceptions/constants/ExceptionConstants.java
+++ src/main/java/com/anderson/address_api/core/exceptions/constants/ExceptionConstants.java
@@ -3,4 +3,6 @@ package com.anderson.address_api.core.exceptions.constants;
 public class ExceptionConstants {
     public final static String ADDRESS_ALREADY_REGISTERED = "Address already registered.";
     public final static String ADDRESS_NOT_FOUND = "Address not found.";
+    public final static String ZIP_CODE_DOES_NOT_EXIST = "Zip code does not exist.";
+    public static final String INTERNAL_SERVER_ERROR = "Internal server error, try later.";
 }
